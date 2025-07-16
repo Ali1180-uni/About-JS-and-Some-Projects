bb9af8142c3d18a5585b07e0ff5b1feb7ebae233
Ali1180-uni <24ntucsfl1180@student.ntu.edu.pk>
2025-07-16 15:09:06 +0500

    Adding about Axios

--- JS/app.js
+++ JS/app.js
@@ -2,18 +2,10 @@ let url = "https://catfact.ninja/fact";
 
 async function getAPI(){
     try{
-    let res = await fetch(url); // await always use with promises and used in async function
-    let data = await res.json(); // using await always in this call otherwise it stopped
-    console.log("Data is : ", data.fact);
-
-    let res1 = await fetch(url); 
-    let data1 = await res1.json();
-    console.log("Data is : ", data1.fact);
-
-    }catch(err){
-        console.log("Error ", err);
-        console.log("Data not shown");
+        let res = await axios.get(url);
+        console.log("The response is: " , res.data.fact);
     }
-
-    console.log("After data");
+    catch(err){
+        console.log("Error is : ", err);
+    }
 }
