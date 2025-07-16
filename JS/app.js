fd582f87e962fff2a131205a4883c43c87430448
Ali1180-uni <24ntucsfl1180@student.ntu.edu.pk>
2025-07-16 14:34:45 +0500

    print fact

--- JS/app.js
+++ JS/app.js
@@ -4,11 +4,11 @@ async function getAPI(){
     try{
     let res = await fetch(url); // await always use with promises and used in async function
     let data = await res.json(); // using await always in this call otherwise it stopped
-    console.log("Data is : ", data);
+    console.log("Data is : ", data.fact);
 
     let res1 = await fetch(url); 
     let data1 = await res1.json();
-    console.log("Data is : ", data1);
+    console.log("Data is : ", data1.fact);
 
     }catch(err){
         console.log("Error ", err);
